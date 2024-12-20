89a2fa12b1090ea548578e4dc8fce8f01cb68748
AKİF GÜLDEMİR <akif.guldemir@ozyer.com>
2024-12-20 10:26:54 +0300

    update

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -2,24 +2,27 @@ import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Login from "./pages/Login/login";
 import Home from "./pages/home/home";
 import ProtectedComponent from "./pages/protectedComponent/protectedComponent";
+import PublicPage from "./pages/publicpages/publicPage";
 import { authAtom } from './recoil/atoms/authAtom';
 import { useRecoilValue } from 'recoil';
 
-const Layout = () => {
+const ProtectedLayout = () => {
   const authState = useRecoilValue(authAtom);
   if (authState.isAuthenticated) {
-    return (
-        <Outlet />
-    );
+    return <Outlet />;
   } else {
     return <Login />;
   }
 };
 
+const PublicLayout = () => {
+  return <Outlet />;
+};
+
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Layout />,
+    element: <ProtectedLayout />,
     children: [
       {
         path: "/",
@@ -35,9 +38,20 @@ const router = createBrowserRouter([
     path: "/login",
     element: <Login />,
   },
+  {
+    path: "/public",
+    element: <PublicLayout />,
+    children: [
+      {
+        path: "/public",
+        element: <PublicPage />,
+      }
+    ],
+  },
 ]);
 
 const AppRoutes = () => {
   return <RouterProvider router={router} />;
 };
+
 export default AppRoutes;
