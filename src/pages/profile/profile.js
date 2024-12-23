d87946a4cda00bd808e06ce975653d6e7404a65b
AKİF GÜLDEMİR <akif.guldemir@ozyer.com>
2024-12-23 14:04:53 +0300

    remove unused imports in Profile component for cleaner code

--- src/pages/profile/profile.js
+++ src/pages/profile/profile.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { useQuery } from 'react-query';
-import useAuth from '../../hooks/useAuth';
-import { useNavigate } from 'react-router-dom';
 import UserService from '../../services/UserService';
 import { userAtom } from '../../recoil/atoms/userAtom';
 import { useRecoilState } from 'recoil';
