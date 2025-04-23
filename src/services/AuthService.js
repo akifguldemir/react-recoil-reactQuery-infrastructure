affe4b72b41851276a6333cc613750fb5ecdcdb9
Akif Güldemir <akifguldemir@gmail.com>
2025-04-24 00:12:31 +0300

    update

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -2,7 +2,7 @@ import BaseService from './BaseService';
 
 class AuthService extends BaseService {
   login(credentials) {
-    return this.post('https://localhost:44373/api/Auth/login', credentials);
+    return this.post('https://dummyjson.com/auth/login', credentials);
   }
 
   logout() {
